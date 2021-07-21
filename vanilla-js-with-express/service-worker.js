829b3d151bd1159ef87e09f096721a57d2f5dbc9
abitshaken <30978672+abitshaken@users.noreply.github.com>
2021-07-21 11:26:18 -0500

    Update tulo-js to version 0.0.4

--- vanilla-js-with-express/service-worker.js
+++ vanilla-js-with-express/service-worker.js
@@ -18,10 +18,9 @@ const staticCacheSpec = {
 
 const imageCacheSpec = {
   name: 'imageCache'+version, //give your cache a name and tag on the version number
-  types: ['image/png'], //input HTML MIME types e.g. text/html, text/css, image/gif, etc.
+  types: ['image'], //input HTML MIME types e.g. text/html, text/css, image/gif, etc.
   urls: ['/catsnakes.png'], //input any reachable file paths to be cached that correspond to the types specific to this cacheSpec
-  strategy: 'NetworkOnly', //currently supported strategies are: CacheFirst, NetworkFirst, NetworkOnly
-  // expiration: 60*60*1000 //in miliseconds: 60*1000 = 1 minute, 20*60*1000 = 20 minutes - this field is OPTIONAL - if omitted, these urls will be refreshed when       the service worker restarts
+  strategy: 'NetworkOnly' //currently supported strategies are: CacheFirst, NetworkFirst, NetworkOnly
 }
 
 cacheGenerator([pageCacheSpec, imageCacheSpec, staticCacheSpec]);//include your page/markup caches first
